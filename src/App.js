da064f4479b3c75abca2e0ce63e96d9680019550
Timur Satiev <60050123+tipask245@users.noreply.github.com>
2023-10-08 22:21:31 +0300

    Revert "reg and auth forms"

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import './App.css';
-import PrivateRouter from './components/PrivateRouter';
 import About from './components/pages/About';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Auth from './components/pages/Auth'
-import Reg from './components/pages/Reg'
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<About/>}/>
-        <Route path='/auth' element={<Auth/>}/>
-        <Route path='/registration' element={<Reg/>}/>
-        <Route element={<PrivateRouter/>}> 
-          <Route>
-            <Route path='/lk' element={<About/>}/>
-          </Route>
-        </Route>
-      </Routes>
-    </Router>
+    <div className="App">
+      <About/>
+      
+    </div>
   );
 }
 
