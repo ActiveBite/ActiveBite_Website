f757dd2a6454ba35f40bcc23c8be5b54b0e8c7f3
Довран Молла-Мурадов <dovran03@mail.ru>
2023-10-08 19:46:23 +0300

    clear

--- src/components/pages/Reg.jsx
+++ src/components/pages/Reg.jsx
@@ -3,16 +3,16 @@ import "./Reg.css"
 
 function Reg() {
   return (
-    <div className="form_Content">
+    <div className="Form_Content">
       <h2> Регистрация</h2>  
       <form>
-        <input type="text" className="fadeIn_second" name="login" placeholder="Логин"/>
-        <input type="email" className="fadeIn_third" name="email" placeholder="Почта"/>
-        <input type="password" className="fadeIn_third" name="password" placeholder="Пароль"/>
-        <input type="password" className="fadeIn_third" name="rep_password" placeholder="Подтверждение пароля"/>
-        <input type="submit" class="fadeIn_fourth" value="Зарегистрироваться"/>
+        <input type="text" className="FadeIn_Second" name="login" placeholder="Логин"/>
+        <input type="email" className="FadeIn_Third" name="email" placeholder="Почта"/>
+        <input type="password" className="FadeIn_Third" name="password" placeholder="Пароль"/>
+        <input type="password" className="FadeIn_Third" name="rep_password" placeholder="Подтверждение пароля"/>
+        <input type="submit" class="FadeIn_Fourth" value="Зарегистрироваться"/>
       </form>
-      <div className="form_Footer">
+      <div className="Form_Footer">
         <a1>Есть аккаунт?</a1><a href="http://localhost:3000/Auth">Авторизироваться</a>
       </div>
       </div>
