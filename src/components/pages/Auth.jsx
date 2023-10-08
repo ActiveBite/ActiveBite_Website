29ce3a93c7f50aa63a8c5a99c165ae3e0eb9c1cf
Довран Молла-Мурадов <dovran03@mail.ru>
2023-10-08 19:38:42 +0300

    clear code

--- src/components/pages/Auth.jsx
+++ src/components/pages/Auth.jsx
@@ -2,19 +2,16 @@ import React from 'react'
 import "./Reg.css"
 
 function Auth() {
-  return (
-    <div className="wrapper_fade_In_Down">
-    <div className="form_Content">
-      <h2 className="active"> Авторизация</h2>
+  return (    <div className="form_Content_Auth">
+      <h2> Авторизация</h2>
       <form>
         <input type="text" className="fadeIn_second" name="login" placeholder="Логин"/>
-        <input type="password" className="fadeIn_third" name="login" placeholder="Пароль"/>
+        <input type="password" className="fadeIn_third" name="password" placeholder="Пароль"/>
         <input type="submit" class="fadeIn_fourth" value="Авторизоваться"/>
       </form>
       <div className="form_Footer">
-        <a>Нет аккаунта?</a><a1 className="under_line_Hover" href="http://localhost:3000/Registration">Зарегистрироваться</a1>
+        <a>Нет аккаунта?</a><a1 href="http://localhost:3000/Registration">Зарегистрироваться</a1>
 
-      </div>
       </div>
       </div>
   )
--- src/components/pages/Reg.jsx
+++ src/components/pages/Reg.jsx
@@ -3,25 +3,19 @@ import "./Reg.css"
 
 function Reg() {
   return (
-    <div className="wrapper_fade_In_Down">
     <div className="form_Content">
-      <h2 className="active"> Регистрация</h2>  
+      <h2> Регистрация</h2>  
       <form>
         <input type="text" className="fadeIn_second" name="login" placeholder="Логин"/>
-        <br />
-        <input type="email" className="fadeIn_third" name="login" placeholder="Почта"/>
-        <br />
-        <input type="password" className="fadeIn_third" name="login" placeholder="Пароль"/>
-        <br />
-        <input type="password" className="fadeIn_third" name="login" placeholder="Подтверждение пароля"/>
-        <br />
+        <input type="email" className="fadeIn_third" name="email" placeholder="Почта"/>
+        <input type="password" className="fadeIn_third" name="password" placeholder="Пароль"/>
+        <input type="password" className="fadeIn_third" name="rep_password" placeholder="Подтверждение пароля"/>
         <input type="submit" class="fadeIn_fourth" value="Зарегистрироваться"/>
       </form>
       <div className="form_Footer">
-        <a1>Есть аккаунт?</a1><a className="under_line_Hover" href="http://localhost:3000/Auth">Авторизироваться</a>
-      </div>,
+        <a1>Есть аккаунт?</a1><a href="http://localhost:3000/Auth">Авторизироваться</a>
       </div>
-    </div>
+      </div>
     )
 }
 export default Reg
