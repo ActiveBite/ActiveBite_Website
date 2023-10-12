345a5b5d35a8860987c1f03c514d267b77d24375
wxnterfield <100841376+wxnterfield@users.noreply.github.com>
2023-10-12 14:04:20 +0000

    React hook form for Auth and Reg

--- src/components/pages/Auth.jsx
+++ src/components/pages/Auth.jsx
@@ -1,15 +1,38 @@
 import React from 'react'
 import "./AuthRegForm.css"
 import { Link } from 'react-router-dom'
+import { useForm, Controller } from 'react-hook-form';
 
 function Auth() {
+    const { handleSubmit, control, errors } = useForm();
+
+    const onSubmit = (data) => {
+        console.log(data);
+      }
+
   return (
     <div className="form_page">
         <div className="form_container auth_form_container">
             <h2>Авторизация</h2>  
-            <form className='form'>
-                <input type="text" className="form_input" placeholder="Логин"/>
-                <input type="password" className="form_input" placeholder="Пароль"/>
+            <form className='form' onSubmit={handleSubmit(onSubmit)}>
+                <Controller
+                    name="login"
+                    control={control}
+                    defaultValue=""
+                    render={({ field }) => (
+                        <input type="text" className="form_input" placeholder="Логин" {...field} />
+                    )}
+                />
+                {errors.login && <p>Поле "Логин" обязательно</p>}
+                <Controller
+                    name="password"
+                    control={control}
+                    defaultValue=""
+                    render={({ field }) => (
+                        <input type="password" className="form_input" placeholder="Пароль" {...field} />
+                    )}
+                />
+                {errors.password && <p>Поле "Пароль" обязательно</p>}
                 <button type="submit" class="form_submit_button">Авторизироваться</button>
             </form>
             <div className="form_footer">
--- src/components/pages/Reg.jsx
+++ src/components/pages/Reg.jsx
@@ -1,23 +1,55 @@
 import React from 'react'
 import "./AuthRegForm.css"
 import { Link } from 'react-router-dom'
-import { useFor, Controller } from 'react-hook-form'
-
-
-
+import { useForm, Controller } from 'react-hook-form'
 
 
 function Reg() {
+
+    const { handleSubmit, control, errors } = useForm();
+    console.log(data);
 
   return (
     <div className="form_page">
         <div className="form_container">
         <h2> Регистрация</h2>  
-        <form className='form'>
-            <input type="text" className="form_input" placeholder="Логин"/>
-            <input type="email" className="form_input" placeholder="Почта"/>
-            <input type="password" className="form_input" placeholder="Пароль"/>
-            <input type="password" className="form_input" placeholder="Подтверждение пароля"/>
+        <form className='form' onSubmit={handleSubmit(onSubmit)}>
+            <Controller
+                name="login"
+                control={control}
+                defaultValue=""
+                render={({ field }) => (
+                    <input type="text" className="form_input" placeholder="Логин" {...field} /> 
+                )}
+            />
+            {errors.login && <p>Поле "Логин" обязательно</p>}
+            <Controller
+                name="email"
+                control={control}
+                defaultValue=""
+                render={({ field }) => (
+                    <input type="email" className="form_input" placeholder="Почта" {...field} />
+                )}
+            />
+            {errors.email && <p>Поле "Почта" обязательно</p>}
+            <Controller
+                name="password"
+                control={control}
+                defaultValue=""
+                render={({ field }) => (
+                    <input type="password" className="form_input" placeholder="Пароль" {...field} />
+                )}
+            />
+           {errors.password && <p>Поле "Пароль" обязательно</p>}
+           <Controller
+                name="confirmPassword"
+                control={control}
+                defaultValue=""
+                render={({ field }) => (
+                    <input type="password" className="form_input" placeholder="Подтверждение пароля" {...field} />
+                )}
+            />
+          {errors.confirmPassword && <p>Поле "Подтверждение пароля" обязательно</p>}
             <button type="submit" class="form_submit_button">Зарегистрироваться</button>
         </form>
         <div className="form_footer">
