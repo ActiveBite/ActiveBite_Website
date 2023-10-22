00d8c9bf95918bb6da55d7996ed7811f7ae5a73c
DAVR18 <145141059+DAVR18@users.noreply.github.com>
2023-10-22 19:50:14 +0000

    error correction

--- src/components/pages/Lk.jsx
+++ src/components/pages/Lk.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
 import "./Lk.css"
 
-function List_components() {
+function ListComponent() {
   return (
       <li>
-      <img className='avatar_list' src="" alt="" />
-      <div>
-        <body className='list_description'>
+        <img className='image_item' src="" alt="" />
+        <div className='general_information'>
           <h6 className='training_title'>Заголовок Тренировки</h6>
           <p className='short_description'>Краткое описание тренировки Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
-        </body>
-      </div>
+        </div>
     </li>
   )   
 }
@@ -18,33 +16,33 @@ function List_components() {
 function Lk() {
   return (
     <div className='user'>
-    <div className='lk_header'>
-      <div className='avatar_header'>
-        <img  className='avatar' src="" alt="" />
-        <h3 className='username'>username</h3>
+      <div className='lk_header'>
+        <div className='avatar_header'>
+          <img  className='avatar' src="" alt="" />
+          <h3 className='username'>username</h3>
+        </div>
+        <div className='lk_buttons_wrap'>
+          <button className='button_header'>Рецепты</button>
+          <button className='button_header'>Тренировки</button>
+        </div>
       </div>
-      <div className='lk_buttons_wrap'>
-        <button className='button_header'>Рецепты</button>
-        <button className='button_header'>Тренировки</button>
+      <div>
+        <h2 className='choisen_buttons_wrap'>Тренировки</h2>
+          <div className='list_button'>
+            <div className='two_button'>
+              <button className='button'>Избранные</button>
+              <button className='button'>Опубликованные</button>
+            </div>
+            <button className='button'>Добавить</button>
+          </div>    
+        <div className='selected_list'>
+          <ul className='components_of_the_list'>
+            <ListComponent />
+            <ListComponent /> 
+            <ListComponent />      
+          </ul>
+        </div>
       </div>
-    </div>
-    <div className='service_block'>
-      <h2 className='choisen_block'>Тренировки</h2>
-        <div className='list_button'>
-          <div className='two_button'>
-            <button className='button'>Избранные</button>
-            <button className='button'>Опубликованные</button>
-          </div>
-          <button className='button'>Добавить</button>
-        </div>    
-      <div className='list_choisen_block'>
-        <ul className='components_of_the_list'>
-          <List_components />
-          <List_components /> 
-          <List_components />      
-        </ul>
-      </div>
-    </div>
    </div>
   )
 }
