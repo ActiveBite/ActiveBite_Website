fe3303aa318350440400c66c4bced4bf82e9c13c
DAVR18 <145141059+DAVR18@users.noreply.github.com>
2023-10-22 14:00:42 +0000

    code upgrade

--- src/components/pages/Lk.jsx
+++ src/components/pages/Lk.jsx
@@ -1,6 +1,20 @@
 import React from 'react'
 import "./Lk.css"
 
+function List_components() {
+  return (
+      <li>
+      <img className='avatar_list' src="" alt="" />
+      <div>
+        <body className='list_description'>
+          <h6 className='training_title'>Заголовок Тренировки</h6>
+          <p className='short_description'>Краткое описание тренировки Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+        </body>
+      </div>
+    </li>
+  )   
+}
+
 function Lk() {
   return (
     <div className='user'>
@@ -16,45 +30,22 @@ function Lk() {
     </div>
     <div className='service_block'>
       <h2 className='choisen_block'>Тренировки</h2>
-      <div className='list_button'>
-      <div className='two_button'>
-        <button className='button'>Избранные</button>
-        <button className='button'>Опубликованные</button>
-      </div>
-        <button className='button'>Добавить</button>
-      </div>
+        <div className='list_button'>
+          <div className='two_button'>
+            <button className='button'>Избранные</button>
+            <button className='button'>Опубликованные</button>
+          </div>
+          <button className='button'>Добавить</button>
+        </div>    
       <div className='list_choisen_block'>
-        <ul>
-          <li className='component_of_the_list'>
-            <img className='avatar_list' src="" alt="" />
-            <div className='list_description'>
-              <h4 className='training_title'>Заголовок тренировки</h4>
-              <h6 className='short_description'>Краткое описание тренировки Lorem ipsum dolor sit amet, consectetur adipiscing elit...</h6>
-            </div>
-          </li>
-          <li className='component_of_the_list'>
-            <img className='avatar_list' src="" alt="" />
-            <div className='list_description'>
-              <h4 className='training_title'>Заголовок тренировки</h4>
-              <h6 className='short_description'>Краткое описание тренировки Lorem ipsum dolor sit amet, consectetur adipiscing elit...</h6>
-            </div>
-          </li>
-          <li className='component_of_the_list'>
-            <img className='avatar_list' src="" alt="" />
-            <div className='list_description'>
-              <h4 className='training_title'>Заголовок тренировки</h4>
-              <h6 className='short_description'>Краткое описание тренировки Lorem ipsum dolor sit amet, consectetur adipiscing elit...</h6>
-            </div>
-          </li>
+        <ul className='components_of_the_list'>
+          <List_components />
+          <List_components /> 
+          <List_components />      
         </ul>
       </div>
     </div>
-    
-    
-          
-
    </div>
-
   )
 }
 
