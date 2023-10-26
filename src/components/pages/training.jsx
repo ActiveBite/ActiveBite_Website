21234e3bcd0c079fe0e9124fd4e3d05c6ede9428
DAVR18 <145141059+DAVR18@users.noreply.github.com>
2023-10-26 11:06:52 +0000

    create training page

---
+++ src/components/pages/training.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function training() {
+  return (
+    <div>
+        <div className='training_header'>
+            <img className='training_image'src="" alt="" />
+            <div>
+                <h2>Заголовок тренировки</h2>
+                <button className='button'>В избранное</button>
+                <button className='button'>Лайк</button>
+                <button className='button'>Дизлайк</button>
+            </div>
+        </div>
+        <div>
+            <p>klsdvmksldmv</p>
+        </div>
+        <div>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default training
