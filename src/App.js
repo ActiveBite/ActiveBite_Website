7b90178bd12bfbc2d5ffcf351a589885b113b693
DAVR18 <145141059+DAVR18@users.noreply.github.com>
2023-10-31 20:08:54 +0000

    troubleshooting errors

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 import PrivateRouter from './components/PrivateRouter';
 import About from './components/pages/About';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Training from './components/pages/Training'
+import Lk from './components/pages/Lk';
+import Training from './components/pages/Training';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path='/' element={<About/>}/>   
         <Route element={<PrivateRouter/>}> 
           <Route>
+            <Route path='/lk' element={<Lk/>}/>
             <Route path='/training' element={<Training/>}/>
           </Route>
         </Route>
--- src/components/pages/Exercise.jsx
+++ src/components/pages/Exercise.jsx
@@ -4,10 +4,8 @@ import "./Exercise.css"
 function Exercise() {
     return (
         <li className='list_item'>
-            <div className='item_wrapper'>
-                <p>Название упражнения</p>
-                <p>Длительность: 5 мин</p>
-            </div>
+            <p>Название упражнения</p>
+            <p>Длительность: 5 мин</p>
         </li>
     )
   }
