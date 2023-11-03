ed466a75f72caa4b2b18685f2af2fdbc730703ec
DAVR18 <145141059+DAVR18@users.noreply.github.com>
2023-11-03 20:32:00 +0000

    error correction

--- src/components/pages/Training.jsx
+++ src/components/pages/Training.jsx
@@ -8,8 +8,8 @@ function Training() {
         <div className='training_header'>
             <img className='training_image'src="" alt="" />
             <div>
-                <h2 className='workout_title'>Заголовок тренировки</h2>
-                <div className='workout_button'>
+                <h2 className='training_title'>Заголовок тренировки</h2>
+                <div className='training_button'>
                     <button className='action_button'>В избранное</button>
                     <button className='action_button'>Лайк</button>
                     <button className='action_button'>Дизлайк</button>
@@ -17,7 +17,7 @@ function Training() {
             </div>
         </div>
         <p className='training_description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sodales ultricies tortor, vel euismod ligula condimentum nec. Curabitur nec orci ante. Donec lectus mauris, sodales a ligula vestibulum, tristique tempus nisl. Aliquam vitae purus quis justo cursus aliquam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sodales ultricies tortor, vel euismod ligula condimentum nec. Curabitur nec orci ante. Donec lectus mauris, sodales a ligula vestibulum, tristique tempus nisl. Aliquam vitae purus quis justo cursus aliquam. </p>
-        <div className='list_exercises_wrapper'>
+        <div>
             <ul className='list_exercises'>
                 <Exercise />
                 <Exercise />
