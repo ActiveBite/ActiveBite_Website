233bd588f831966f34e33f52157baff1cf62c7c8
DAVR18 <145141059+DAVR18@users.noreply.github.com>
2023-11-20 11:01:03 +0000

    ready modal window

--- src/components/pages/Mw.jsx
+++ src/components/pages/Mw.jsx
@@ -1,17 +1,44 @@
 import React from 'react'
 import "./Mw.css"
 
-function Mw() {
+function Mw({active, setActive}) {
     return (
-        <div className='window_wrapper'>
-            <div className='window'>
-                <h2 className='window_title'>Публикация тренировки</h2>
-                <div className='filling_fields'>
-                    <p><textarea className='title_field' placeholder='Заголовок тренировки'></textarea></p>
-                    <p><textarea className='description_field' placeholder='Описание тренировки'></textarea></p>
-                </div>
-                <div>
-                    
+        <div className={active ? "window active" : "window"} onClick={() => setActive(false)}>
+            <div className='window_body' onClick={e => e.stopPropagation()}>
+                <div className='window_content'>
+                    <h2 className='window_title'>Публикация тренировки</h2>
+                    <div className='filling_fields'>
+                        <input type="text" className='title_field' placeholder='Заголовок тренировки'/>
+                        <p><textarea className='description_field' placeholder='Описание тренировки'></textarea></p>
+                    </div>
+                    <div className='add_picture_wrapper'>
+                        <input type="file" className="picture"/>
+                        <button className='add_picture'>Добавить картинку</button>
+                    </div>
+                    <div className='adding_exercise_wrapper'>
+                        <div className='adding_exercise'>
+                            <input type="text" placeholder='Упражнение' className='exercise_info'/>
+                            <input type="text" placeholder='Длительность' className='exercise_info'/>
+                        </div>
+                        <button className='adding'>Добавить</button>
+                    </div>
+                    <div className='list_exercise_wrapper'>
+                        <ul className='list_exercise'>
+                            <li className='exercise'>
+                                <p>Упражнение</p>
+                                <p>Длительность мин.</p>
+                            </li>
+                            <li className='exercise'>
+                                <p>Анжуманя</p>
+                                <p>5 мин.</p>
+                            </li>
+                            <li className='exercise'>
+                                <p>Бегит</p>
+                                <p>10 мин.</p>
+                            </li>  
+                        </ul>
+                    </div>
+                    <button className='public' id='close_btn'>Опубликовать</button>
                 </div>
             </div>
         </div>
